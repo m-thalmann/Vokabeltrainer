319855f602a768441662951ce1b23b47c49a61bc
MrSmieds <32959373+MrSmieds@users.noreply.github.com>
2017-11-16 18:40:02 +0100

    commit 6

---
+++ src/net/tfobz/vokabeltrainer/model/Example.java
@@ -0,0 +1,62 @@
+package net.tfobz.vokabeltrainer.model;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class Example extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Example frame = new Example();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Example() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNeu = new JButton("Neu");
+		btnNeu.setBounds(149, 103, 97, 25);
+		btnNeu.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JButton n1 = new JButton("Neue Kartei");
+				JButton n2 = new JButton("Neues Fach");
+				contentPane.add(n1);
+				contentPane.add(n2);
+				
+				Un
+				
+				
+			}
+		});
+		contentPane.add(btnNeu);
+	}
+}
